8935c85d72b32e337e6b55e62550f0926e6cc4d3
Leza <1046527915@qq.com>
2023-11-15 12:45:27 +0800

    更新xmake平台判断

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,10 @@ set_project("opengl_learn")
 set_version("1.0.0")
 
 set_languages("c++17")
-set_toolset("clang-cl")
-set_runtimes("MD")
+if is_plat("windows") then
+    set_toolset("clang-cl")
+    set_runtimes("MD")
+end
+
 
 includes("*/xmake.lua")
