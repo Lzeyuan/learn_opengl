1df6ddfaed256fa2548321780cde5348eebe30b6
Leza <1046527915@qq.com>
2023-11-15 12:38:01 +0800

    增加着色器类

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # learn_opengl
+## 介绍
 这个项目两个目的：
 1. 记录[LearnOpenGL](https://learnopengl-cn.github.io/)代码
 2. 记录学习cpp和xmake笔记
 
 markdown样式难看，所以用飞书记录[笔记](https://x5fr84m9vk.feishu.cn/docx/Wp7QdMspeosuEpxSru9csAPYnSc?from=from_copylink)
+
+## 规范
+### 代码格式
+使用clang-format的[google风格](https://zh-google-styleguide.readthedocs.io/en/latest/google-cpp-styleguide/)
+
+C++文件使用`.cpp` `.hpp`后缀
+
+C文件使用`.c` `.h`后缀
--- src/xmake.lua
+++ src/xmake.lua
@@ -5,5 +5,15 @@ add_requires("glfw",  {configs = {vs_runtime ="MD"}})
 target("opengl_xmake")
   set_kind("binary")
   add_files("*.cpp")
+  add_includedirs("/base")
   add_packages("glfw")
   add_deps("glad")
+  before_build(function (target)
+    print("before_build")
+    print("copying shaders from " .. "$(scriptdir)/shaders/" .. " to " .. target:targetdir())
+    -- 先清空原有shaders
+    os.rm(target:targetdir() .. "/shaders/")
+    -- 递归复制当前目录到临时目录
+    -- {rootdir = "src"}保持目录层级
+    os.cp("$(scriptdir)/shaders/", target:targetdir(), {rootdir = "src"})
+  end)
